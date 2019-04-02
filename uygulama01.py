51057eee0b2699c2298269c3af2e42e9f1575301
gmzgmzz <48158647+gmzgmzz@users.noreply.github.com>
2019-04-02 22:09:12 +0300

    Add files via upload

---
+++ uygulama01.py
@@ -0,0 +1,62 @@
+puan= 0
+#soru1
+print("Türkiyede kaç yaşından itibaren ehliyet sahibi olabilirsiniz?")
+cevap=input("Cevabı yazınız: ")
+if cevap=="18":
+    puan+=20
+    print("Cevabınız doğrudur.")
+    print("Mevcut puan durumunuz:"+" "+str(puan))
+else:
+    print("Cevabınız yanlış")
+    print("Bu soru için hiç puan toplayamadınız.")
+    print("Mevcut puan durumunuz:  "+ " "+str(puan))
+#soru2
+print("Haftanın ilk günü nedir?")
+cevap=input("Cevabı yazınız:")
+if cevap=="Pazartesi":
+    puan+=20
+    print("Cevabınız doğrudur.")
+    print("Mevcut puan durumunuz:"+" "+str(puan))
+else:
+    print("Cevabınız yanlış")
+    print("Bu soru için hiç puan toplayamadınız.")
+    print("Mevcut puan durumunuz: "+" "+str(puan))
+#soru3
+print("Kaç mevsim vardır?")
+cevap=input("Cevabı yazınız:")
+if cevap=="12":
+    puan+=20
+    print("Cevabınız doğrudur.")
+    print("Mevcut puan durumunuz: "+" "+str(puan))
+else:
+    print("Cevabınız yanlış")
+    print("Bu soru için hiç puan toplayamadınız.")
+    print("Mevcut puan durumunuz: "+" "+str(puan))
+#soru4
+print("Bir yıl kaç gündür?")
+cevap=input("Cevabı yazınız:")
+if cevap=="365":
+    puan+=20
+    print("Cevabınız doğrudur.")
+    print("Mevcut puan durumunuz: "+" "+str(puan))
+else:
+    print("Cevabınız yanlış")
+    print("Bu soru için hiç puan toplayamdınız.")
+    print("Mevcut puan durumunuz:  "+"  "+str(puan))
+#soru5
+print("Kapadokya hangi ilimizde bulunur?")
+cevaap=input("Cevabı yazınız:")
+if cevap=="Nevşehir":
+    puan+=20
+    print("Cevabınız doğrudur.")
+    print("Mevcut puan durumunuz: "+" "+str(puan))
+else:
+    print("Cevabınız yanlış")
+    print("Bu soru için hiç puan toplayamadınız.")
+    print("Mevcut puan durumunuz:  "+" "+str(puan))
+Son="Sınavımız bitmiştir.Kazanılan puan: "
+print("Son")
+
+
+
+
